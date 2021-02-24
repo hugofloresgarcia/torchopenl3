14756e1d94b2b2c7f774edce318aabcf45356e76
Hugo Flores Garcia <hf01049@georgiasouthern.edu>
2021-02-23 19:38:50 -0500

    fix assets

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setup(
         'audio_utils @ git+https://github.com/hugofloresgarcia/audio-utils'
     ],
     packages=['torchopenl3'],
-    package_data={'': ['assets/*']},
+    package_data={'torchopenl3': ['assets/*']},
+    include_package_data=False,
     long_description=long_description,
     long_description_content_type='text/markdown',
     keywords=[],
